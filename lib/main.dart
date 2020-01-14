e04a366d6da636d6f6ede2da38b7fb6f444fa812
Musab Agab <musabagab2010@gmail.com>
2020-01-14 19:18:34 +0300

    Added spash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wallpapery App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+    return new SplashScreen(
+        seconds: 5,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text(
+          'Welcome In SplashScreen',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Wallpapers Here'),
-          ),
+        image: Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter SpashScreen"),
+        loaderColor: Colors.red);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Welcome In SplashScreen Package"),
+          automaticallyImplyLeading: false),
+      body: new Center(
+        child: new Text(
+          "Done!",
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
         ),
       ),
     );
