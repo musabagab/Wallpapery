f67abf2778d1f5e5f2b294307b913b46eff093ac
Musab Agab <musabagab2010@gmail.com>
2020-01-14 18:18:35 +0300

    Testing app

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            child: Text('Hello World'),
+            child: Text('Wallpapers Here'),
           ),
         ),
       ),
