29c2d9d539ae1320006455ca48c3525eebd636e5
Musab Agab <musabagab2010@gmail.com>
2020-01-15 09:12:09 +0300

    Added Simple CarouselSlider

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:wallpapery/widgets/SearchBar.dart';
 
@@ -16,6 +17,30 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             SearchBar(),
+            SizedBox(
+              height: 20,
+            ),
+            CarouselSlider(
+              height: 400.0,
+              enlargeCenterPage: true,
+              viewportFraction: .7,
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          'text $i',
+                          style: TextStyle(fontSize: 16.0),
+                        ));
+                  },
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
