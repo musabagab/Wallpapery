f7df55512e37ed51b337dac58dc8e65e15904f83
Musab Agab <musabagab2010@gmail.com>
2020-01-17 11:33:41 +0300

    Added the back arrow to deatilsview

--- lib/ui/details_view.dart
+++ lib/ui/details_view.dart
@@ -12,15 +12,42 @@ class DetailsView extends StatelessWidget {
     return BaseView<DetailsModel>(
       onModelReady: (model) => model.setDeatilsHit(selectedHit),
       builder: (context, child, model) => SafeArea(
-        child: Hero(
-          tag: model.selectedHit.largeImageUrl,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              model.selectedHit.largeImageUrl,
-              fit: BoxFit.fill,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              child: Hero(
+                tag: model.selectedHit.largeImageUrl,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    model.selectedHit.largeImageUrl,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white54),
+                child: GestureDetector(
+                  onTap: () {
+                    // navifate back
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                    size: 25,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
