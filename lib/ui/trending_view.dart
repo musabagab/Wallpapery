a1ff1e2a5cac83d176825470c3a7f1b4e407a6e1
Musab Agab <musabagab2010@gmail.com>
2020-01-20 21:44:50 +0300

    added searchbar in trendingview

--- lib/ui/trending_view.dart
+++ lib/ui/trending_view.dart
@@ -3,16 +3,20 @@ import 'package:wallpapery/scoped_models/trending_model.dart';
 import 'package:wallpapery/service_locator.dart';
 import 'package:wallpapery/services/api_service.dart';
 import 'package:wallpapery/ui/base_view.dart';
+import 'package:wallpapery/widgets/SearchBar.dart';
 
 class TrendingView extends StatelessWidget {
+  final String category = locator<ApiService>().category;
   @override
   Widget build(BuildContext context) {
-    print(locator<ApiService>().category);
     return BaseView<TrendingModel>(
       builder: (context, child, model) => Scaffold(
-        body: Center(
-          child: Text(
-            this.runtimeType.toString(),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 16, bottom: 16),
+          child: Column(
+            children: <Widget>[
+              SearchBar(),
+            ],
           ),
         ),
       ),
